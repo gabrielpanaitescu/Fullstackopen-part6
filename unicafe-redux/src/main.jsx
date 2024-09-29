127591f9a3b3c6aa24aba5c752ac260162d5e620
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2024-09-29 17:45:53 +0300

    Complete exercises 6.1 and 6.2

---
+++ unicafe-redux/src/main.jsx
@@ -0,0 +1,29 @@
+import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
+import counterReducer from "./reducer";
+
+const store = createStore(counterReducer);
+
+const App = () => {
+  const state = store.getState();
+
+  return (
+    <div>
+      <button onClick={() => store.dispatch({ type: "GOOD" })}>good</button>
+      <button onClick={() => store.dispatch({ type: "OK" })}>ok</button>
+      <button onClick={() => store.dispatch({ type: "BAD" })}>bad</button>
+      <button onClick={() => store.dispatch({ type: "RESET" })}>reset</button>
+      <div>good {state.good}</div>
+      <div>ok {state.ok}</div>
+      <div>bad {state.bad}</div>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const renderApp = () => {
+  root.render(<App />);
+};
+
+renderApp();
+store.subscribe(renderApp);
